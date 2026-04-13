e0aa8ba2ddb2671e1cd6f0871061968540f3ffbe
Kiony Mussagy <kionydalenamussagymascarenhas.133097@marwadiuniversity.ac.in>
2026-04-13 13:45:18 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,25 @@
+import Chat from "./Chat";
+export default function Home() {
+  return (
+    <div className="home">
+
+      { }
+      <div className="hero">
+        <h1>Welcome to ChocoDelicias 🍰</h1>
+        <p>Fresh & sweet cakes everyday ❤️</p>
+      </div>
+
+      { }
+      <div className="home-image">
+        <img 
+          src="/cake.jpg" 
+          alt="cakes"
+        />
+      </div>
+
+      
+        <Chat />
+
+    </div>
+  );
+}
